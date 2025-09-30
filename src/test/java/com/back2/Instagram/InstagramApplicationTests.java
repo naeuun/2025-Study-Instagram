2ff01f37a033fb58d77553faa6303ed36c879b9c
naeun <1210naeun@gmail.com>
2025-10-01 00:23:58 +0900

    ⚙️  setting

---
+++ src/test/java/com/back2/Instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back2.Instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
